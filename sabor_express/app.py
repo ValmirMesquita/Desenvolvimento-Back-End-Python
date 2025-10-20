3ff260cc10bd89e096573bd6443938f2f88a71bd
Valmir L Sousa Mesquita <valmirsmesquita@gmail.com>
2025-10-20 19:19:19 -0300

    Update app.py

--- sabor_express/app.py
+++ sabor_express/app.py
@@ -1,33 +1,55 @@
 import os # Biblioteca para interagir com o sistema operacional
 
-
-os.system('cls' if os.name == 'nt' else 'clear') # Limpa a tela do terminal
-print("""
-     
+# Função para exibir o cabeçalho do aplicativo
+def exibir_cabecalho():
+    
+    print("""
+        
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
 ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
 ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
-      \n""")
-
-print("1. Cadastrar restaurante")
-print("2. Listar restaurantes")
-print('3. Ativar restaurante')
-print("4. Sair\n")
+\n""")
 
 
+ # Função para finalizar o aplicativo
+def finalizar_app():
+    os.system('cls')
+    print("Finalizando o aplicativo...")
 
-opcao_escolhida = int(input("Escolha uma opção: "))
-print(f"A opçao escolhida foi: {opcao_escolhida}")
-if opcao_escolhida == 1:
-    print("Cadastrar restaurante")
-elif opcao_escolhida == 2:
-    print("Listar restaurantes")
-elif opcao_escolhida == 3:
-    print("Ativar restaurante")
-elif opcao_escolhida == 4:
-    print("Sair")
-else:
-    print("Opção inválida")
+def exibir_opcao():   
+    print("1. Cadastrar restaurante")
+    print("2. Listar restaurantes")
+    print('3. Ativar restaurante')
+    print("4. Sair\n")
+    print("=====================================")
+
+
+def escolher_opcao():          
+    opcao_escolhida = int(input("Escolha uma opção: "))
+    
+    print(f"A opçao escolhida foi: {opcao_escolhida}")
+    if opcao_escolhida == 1:
+        print("Cadastrar restaurante")
+    elif opcao_escolhida == 2:
+        print("Listar restaurantes")
+    elif opcao_escolhida == 3:
+        print("Ativar restaurante")
+    elif opcao_escolhida == 4:
+        finalizar_app()
+    else:
+        print("Opção inválida")
+ 
+# Função principal do aplicativo   
+def main():
+    # Aqui você pode adicionar mais funcionalidades conforme necessário
+    exibir_cabecalho()
+    exibir_opcao()
+    escolher_opcao()
+   
+        
+# Executa a função principal se o script for executado diretamente   
+if __name__== "__main__":
+    main()
