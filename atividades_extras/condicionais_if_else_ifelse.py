a8a1aef5b5ba756a9d54117e5fd370d2abdcaae7
Fulano de Tal <valmirsmesquita@gmail.com>
2025-11-10 17:20:53 -0300

    atualizacao de condicionais

---
+++ atividades_extras/condicionais_if_else_ifelse.py
@@ -0,0 +1,12 @@
+# Condicionais if, else e elif
+
+#nome = "Start"
+nome = "Alura"
+#nome = "Latam"
+
+if nome == "Alura":
+    print("Boas vindas a Alura")  
+elif nome == "Latam":
+    print("Bem vindo a latam")
+else:
+    print("Nome desconhecido")
