506d81ada03870a9a4ed8b824d9fdec753a7a441
Fulano de Tal <valmirsmesquita@gmail.com>
2025-11-29 19:57:26 -0300

    fucoes

---
+++ atividades_extras/funcoes/funcao_ola_mundo.py
@@ -0,0 +1,4 @@
+def ola_mundo(nome):
+    return f"Olá, {nome}!"
+# Exemplo de uso
+print(ola_mundo("Mundo"))  # Saída: Olá, Mundo! 
--- atividades_extras/praticando_lacos_for_while/calculando_soma_numeros_lista.py
+++ atividades_extras/praticando_lacos_for_while/calculando_soma_numeros_lista.py
@@ -1,4 +1,7 @@
-listas_valores={20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120}
+numeros = {20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120}
 
-for valor in listas_valores:
-    print(valor+valor)
+soma = 0
+for numero in numeros:
+    soma += numero
+
+print(f"A soma dos números na lista é: {soma}")
