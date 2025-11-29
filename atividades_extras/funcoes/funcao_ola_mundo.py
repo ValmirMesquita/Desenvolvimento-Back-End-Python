73d5f7da41ee45899474240711dc8a35192aa0ea
Fulano de Tal <valmirsmesquita@gmail.com>
2025-11-29 20:06:21 -0300

    fucoes

--- atividades_extras/funcoes/funcao_ola_mundo.py
+++ atividades_extras/funcoes/funcao_ola_mundo.py
@@ -1,4 +1,15 @@
-def ola_mundo(nome):
+def ola_mundo(nome ):
     return f"Olá, {nome}!"
 # Exemplo de uso
 print(ola_mundo("Mundo"))  # Saída: Olá, Mundo! 
+
+def soma(a, b):
+    soma  = (a + b),(a - b),(a * b),(a / b )
+    return soma
+print(soma(3, 5))  # Saída: 8
+
+def saudacao_personalizada(nome, saudacao):
+    return f"{saudacao}, {nome}!"
+# Exemplo de uso
+print(saudacao_personalizada("Alice", "Bem-vinda"))  # Saída: Bem-vinda, Alice!
+print(saudacao_personalizada("Mario", "Bom-dia"))  # Saída: Bom-dia, Mario!
